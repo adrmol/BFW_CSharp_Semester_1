bcf7cb1a428733133fd6e3578c480e87963f227f
Adrian Molenda <Adrian.Molenda@wibizu.de>
2022-02-16 18:27:46 +0100

    19 fertig

--- BFW_Uebungen_Semester_1/Aufgabe_19.cs
+++ BFW_Uebungen_Semester_1/Aufgabe_19.cs
@@ -25,16 +25,26 @@ namespace BFW_Uebungen_Semester_1
 
             int summe = 0;
             int kzahl = array1[0];
+            int gzahl = array1[0];
             for (int i = 0; i < 5; i++)
             {
                 summe += array1[i]; //summe
-                kzahl = kzahl > array1[i] ? kzahl = array1[i] : kzahl; //kleinste Zahl
-
+                kzahl = kzahl > array1[i] ? array1[i] : kzahl; //kleinste Zahl
+                gzahl = gzahl < array1[i] ? array1[i] : gzahl; //größte Zahl
             }
+
+            //Umwandlung in double für bessere Genaugigkeit
+            double a = array1[0];
+            double b = array1[1];
+            double c = array1[2];
+            double d = array1[3];
+            double e = array1[4];
 
             Console.WriteLine("Die Summe der Zahlen ist " + summe);
             Console.WriteLine("Die kleinste Zahl ist " + kzahl);
-
+            Console.WriteLine("Die größte Zahl ist " + gzahl);
+            Console.WriteLine("Der Mittelwert ist " + (a + b + c + d + e) / 5);
+            
         }
 
         static int conv()
