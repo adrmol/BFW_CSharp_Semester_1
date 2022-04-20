8555c3aaf369488032e3c22af56bb753b3e6408a
adrmol <99812407+adrmol@users.noreply.github.com>
2022-04-20 02:29:49 +0200

    Update Aufgabe_A2.cs
    final

--- Weitere Übungsaufgaben/Aufgabe_A2.cs
+++ Weitere Übungsaufgaben/Aufgabe_A2.cs
@@ -14,14 +14,12 @@ namespace Weitere_Übungsaufgaben
 
             double.TryParse(Console.ReadLine(), out double fallhoehe);
             
-            while (fallhoehe >= 0.02)
+            while (fallhoehe >= 0.01)
             {
-                Console.WriteLine(fallhoehe);
                 fallhoehe /= 2;
                 anzahl++;
             }
 
-            Console.WriteLine(fallhoehe);
             Console.WriteLine("\nDie Anzahl der Sprünge: " + anzahl);
         }
     }
