c5f65be4accd174b37e0bbd35a03fd85a4829b99
Adrian Molenda <Adrian.Molenda@wibizu.de>
2022-05-18 12:46:43 +0200

    b1

--- Weitere Übungsaufgaben/Aufgabe_B1.cs
+++ Weitere Übungsaufgaben/Aufgabe_B1.cs
@@ -9,29 +9,27 @@ namespace Weitere_Übungsaufgaben
         public static void Start()
         {
             bool loop;
-            decimal einkaufsPreis, vermietsPreis;
+            decimal einkaufsPreis, vermietsPreis, monate;
 
-            Console.OutputEncoding = Encoding.UTF8;
             Console.WriteLine("Durchnittlichen Einkaufspreis eingeben: ");
 
             do
-            {
                 if (loop = !decimal.TryParse(Console.ReadLine(), out einkaufsPreis))
                     Console.WriteLine("Falsche Eingabe, bitte nochmal versuchen:");
-            } while (loop);
+            while (loop);
 
             Console.WriteLine("Durchnittlichen Vermietspreis eingeben:");
 
             do
-            {
                 if (loop = !decimal.TryParse(Console.ReadLine(), out vermietsPreis))
                     Console.WriteLine("Falsche Eingabe, bitte nochmal versuchen:");
-            } while (loop);
+            while (loop);
 
-            Console.WriteLine("\nKosten wieder eingenommen nach {0} Monaten ({1} Jahr/e {2} Monat/e)", Math.Ceiling(einkaufsPreis / vermietsPreis) , Math.Floor(einkaufsPreis / vermietsPreis / 12), Math.Ceiling(einkaufsPreis / vermietsPreis % 12));
-            
+            monate = einkaufsPreis / vermietsPreis;
+            Console.WriteLine("\nKosten wieder eingenommen nach {0} Monat/en ({1} Jahr/e {2} Monat/e)", Math.Ceiling(monate), Math.Floor(monate / 12), Math.Ceiling(monate % 12));
+
             for (int i = 5; i <= 20; i += 5)
-                Console.WriteLine("Nach {0} Jahren: {1:N2}", i, Math.Round((vermietsPreis * i * 12 - einkaufsPreis), 2, MidpointRounding.AwayFromZero), i);
+                Console.WriteLine("Nach {0} Jahren: {1:N2}", i, Math.Round(vermietsPreis * i * 12 - einkaufsPreis, 2, MidpointRounding.AwayFromZero), i);
         }
     }
 }
